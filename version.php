06a2b09c012032a29b8ed9425ddbf5600f982c79
Dan Poltawski <dan@moodle.com>
2012-12-19 16:43:34 +0800

    Add missing version info

--- version.php
+++ version.php
@@ -25,6 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2012121800;            // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2012121900;            // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2012112900;            // Requires this Moodle version
 $plugin->component = 'block_spam_deletion'; // Full name of the plugin (used for diagnostics)
+$plugin->release   = '0.1';
+$plugin->maturity  = MATURITY_ALPHA;
