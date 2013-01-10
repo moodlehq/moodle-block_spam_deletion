ecf632221186b1f10c735e8f68d8f93a0dc135dc
Dan Poltawski <dan@moodle.com>
2013-01-10 10:35:22 +0800

    Don't require_strings_for_js twice uncessarily

--- block_spam_deletion.php
+++ block_spam_deletion.php
@@ -81,14 +81,13 @@ class block_spam_deletion extends block_base {
         $this->content = new stdClass;
 
         if (isloggedin() && !isguestuser()) {
+            $this->page->requires->strings_for_js(array('reportasspam'), 'block_spam_deletion');
+            $this->page->requires->js_init_call('M.block_spam_deletion.add_to_comments');
 
             if ($this->page->pagetype == 'mod-forum-discuss') {
                 // Add the JS to put the 'Report as spam' link.
-                $this->page->requires->strings_for_js(array('reportasspam'), 'block_spam_deletion');
                 $this->page->requires->js_init_call('M.block_spam_deletion.add_to_forum_posts');
             }
-            $this->page->requires->strings_for_js(array('reportasspam'), 'block_spam_deletion');
-            $this->page->requires->js_init_call('M.block_spam_deletion.add_to_comments');
         }
 
         if (has_capability('block/spam_deletion:viewspamreport', $this->context)) {
